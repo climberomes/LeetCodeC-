18dfa5b882b5d16dbaacc946baacf84588a4d55a
climberomes <126999678+climberomes@users.noreply.github.com>
2023-04-24 13:35:04 -0500

    Create 6_Zigzag-Conversion_V2.cpp
    very similar code. Did not use hash map this time and the speed and memory went up significantly
    Runtime 45.5% and Memory 44.43%

---
+++ 6_Zigzag-Conversion_V2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1)  
+            return s;
+
+        vector<string> myMap(numRows, "");
+
+        bool direction = false;     //false is down
+        int currRow = 0;
+
+        for(int indx = 0; indx < s.size();indx++)
+        {
+            //insert to hashmap
+            myMap[currRow] += s[indx];
+            
+            //if add row x (x,y)
+            //else subtract row x and add 1 to column y (x,y)
+            (!direction) ? currRow++ : currRow--;
+
+            //Change direction if we exceed lower and positive bounds on next pass
+            if(currRow + 1 >= numRows || currRow - 1 <= -1) {direction = !direction;}
+        }
+        
+        string outStr;
+        for(auto &it : myMap)  outStr += it;
+
+        return outStr;
+    }
+};
+Console
