92070e7ac203bba2f9a697a9d4de593517f53a02
climberomes <126999678+climberomes@users.noreply.github.com>
2023-04-29 21:27:40 -0500

    Update 424_Longest-Repeating-Character-Replacement_V1.cpp

--- 424_Longest-Repeating-Character-Replacement_V1.cpp
+++ 424_Longest-Repeating-Character-Replacement_V1.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         if(s.size()<=k+1){return s.size();}
-        int winStrt = 0, MaxRepElem = 0, ans=0, curr_len = 0;
+        int winStrt = 0, maxReoccurElem = 0, ans=0, curr_len = 0;
         unordered_map<char,int> myMap;                      //Hash map for window
 
         for(int winEnd = 0; winEnd<s.size();winEnd++)
         {
             myMap[s[winEnd]]++;                             //Increase letter count
-            MaxRepElem=max(MaxRepElem,myMap[s[winEnd]]);    //Max between element in window and max we can replace
+            maxReoccurElem=max(MaxRepElem,myMap[s[winEnd]]);    //Max element weve seen reoccur in window
 
-            curr_len=winEnd-winStrt+1;                      //Window Size cant exceed k
-            if(curr_len-MaxRepElem>k)                       //If we exceed K the amount allowed to replace we have gone too far
+            curr_len=winEnd-winStrt+1;                      //Window Size cant exceed k from length of window and max reocurring
+            if(curr_len-MaxRepElem>k)
             {
                 myMap[s[winStrt]]--;                        //Window moves one less letter is allowed
                 winStrt++;                                  //Move window up one
