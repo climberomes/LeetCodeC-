1d7c1199cbfa6434aad23ab01fe5ad86ee02aa8f
climberomes <126999678+climberomes@users.noreply.github.com>
2023-04-30 21:55:01 -0500

    Create 14_Longest-Common-Prefix_V1.cpp
    Runtime
    4 ms
    Beats
    72.14%
    Memory
    9.2 MB
    Beats
    82.55%

---
+++ 14_Longest-Common-Prefix_V1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = strs[0];
+
+        for(int i = 1; i < strs.size();i++)
+        {
+            for(int j = 0; j < ans.size();j++)
+            {
+                if(ans[j] != strs[i][j]) {
+                    ans = ans.substr(0,j);
+                    if(ans.size()==0) return ans;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
