c59df5b47c52c3d395e153721fef269ad76b4595
climberomes <126999678+climberomes@users.noreply.github.com>
2023-04-30 22:42:39 -0500

    Create 283_Move-Zeroes_V2.cpp

---
+++ 283_Move-Zeroes_V2.cpp
@@ -0,0 +1,9 @@
+//Copied solution but i understood the concept fairly quickly\
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums){
+        for(int i=0,next=0;i<nums.size();++i)
+            if(nums[i])
+                swap(nums[i],nums[next++]);
+    }
+};
